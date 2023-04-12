d1714d1b20bbe0c658b02baf20692faed71788e2
Mateus <mochovik.vc@gmail.com>
2023-04-11 23:57:16 -0300

    configuração de módulo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
+import { Database } from './repository/database';
+import { OrderRepository } from './repository/order.repository';
+import { ProductRepository } from './repository/product.repository';
+import { OrderService } from './service/order/order.service';
+import { ProductService } from './service/product/product.service';
+import { ProductAvailableValidator } from './validator/product-available/product-available.validator';
+import { OrderController } from './controller/order/order.controller';
+import { ProductController } from './controller/product/product.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [ConfigModule.forRoot()],
+  controllers: [OrderController, ProductController],
+  providers: [
+    Database,
+    OrderRepository,
+    ProductRepository,
+    OrderService,
+    ProductService,
+    ProductAvailableValidator,
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {}
+}
